fd38e0faa823e0eb6b5c4000c751d39f451bca23
kvanadev <kvanadev1@gmail.com>
2014-06-04 16:01:42 +0530

    Initialize repository

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+#require 'rubygems'
+#require 'bundler/setup'  # require your gems as usual
+#require 'nokogiri'
+
+
+
+Rails.application.initialize!
